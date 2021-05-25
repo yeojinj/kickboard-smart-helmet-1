61ee5486c0264293117cebe70c0b278c32c38c1d
KJE51 <kjenotn@gmail.com>
2021-05-26 00:54:57 +0900

    강제중지됨

--- app/src/main/java/com/example/kickboard_helmet/MainActivity.java
+++ app/src/main/java/com/example/kickboard_helmet/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -14,19 +15,24 @@ import com.google.zxing.integration.android.IntentResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_scan;
     private IntentIntegrator qrScan;
+    private TextView textViewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_scan=findViewById(R.id.btn_scan);
+        btn_scan= (Button) findViewById(R.id.btn_scan);
+        textViewName = (TextView) findViewById(R.id.textViewName);
 
         qrScan = new IntentIntegrator(this);
+
 
         btn_scan.setOnClickListener(new View.OnClickListener() {
 
@@ -54,10 +60,11 @@ public class MainActivity extends AppCompatActivity {
                 //스캔 결과 obj에 저장함
                 JSONObject obj = new JSONObject(result.getContents());
                 //헬멧 번호 다른 변수에 저장->만들어야함
-                //textViewName.setText(obj.getString("helmet"));
+                textViewName.setText(obj.getString("helmet"));
 
             } catch(JSONException e){
                 e.printStackTrace();
+
             }
         }}else{
             super.onActivityResult(requestCode, resultCode, data);
